f65340bd7a90d536a60e78538220100e29b87ff4
Stuart Lees <stuartlees@gmail.com>
2014-08-13 11:51:31 -0400

    Initial integration of Google Drive Picker
    - Updated bower to include google drive picker component
    - Adding gapi-mock.js to provide ability to do mock e2e tests just as its done in Google Drive Picker repo
    - Revised english translation.json to include additional localization
    - Modified directive HTML template to apply "section" styling/layout and also included google drive picker directive and label
    - Updated directive to ensure tooltip and google drive picker are injected as dependencies and also wrapping in self executing anonymous function
    - Updated e2e test HTML file to include appropriate imports for Google Drive Picker integration and also correct formatting

--- src/angular/dtv-spreadsheet-controls.js
+++ src/angular/dtv-spreadsheet-controls.js
@@ -1,38 +1,45 @@
-angular.module("risevision.widget.common.google-spreadsheet-controls")
-  .directive("spreadsheetControls", ["$document", "$window", "$log", "$templateCache",
-    function ($document, $window, $log, $templateCache) {
-    return {
-      restrict: "E",
-      scope: {
-        spreadsheet: "="
-      },
-      template: $templateCache.get("spreadsheet-controls.html"),
-      link: function (scope) {
-        scope.defaultSetting = {
-          cells: "sheet",
-          range: "",
-          sheet: "",
-          headerRow: false,
-          refresh: "60"
-        };
+(function () {
+  "use strict";
 
-        scope.defaults = function(obj) {
-          if (obj) {
-            for (var i = 1, length = arguments.length; i < length; i++) {
-              var source = arguments[i];
-              for (var prop in source) {
-                if (obj[prop] === void 0) {
-                  obj[prop] = source[prop];
+  angular.module("risevision.widget.common.google-spreadsheet-controls",
+    ["risevision.widget.common.translate", "risevision.widget.common.google-drive-picker",
+    "risevision.widget.common.tooltip"])
+    .directive("spreadsheetControls", ["$document", "$window", "$log", "$templateCache",
+      function ($document, $window, $log, $templateCache) {
+      return {
+        restrict: "E",
+        scope: {
+          spreadsheet: "="
+        },
+        template: $templateCache.get("spreadsheet-controls.html"),
+        link: function (scope) {
+          scope.defaultSetting = {
+            cells: "sheet",
+            range: "",
+            sheet: "",
+            headerRow: false,
+            refresh: "60"
+          };
+
+          scope.defaults = function(obj) {
+            if (obj) {
+              for (var i = 1, length = arguments.length; i < length; i++) {
+                var source = arguments[i];
+                for (var prop in source) {
+                  if (obj[prop] === void 0) {
+                    obj[prop] = source[prop];
+                  }
                 }
               }
             }
-          }
-          return obj;
-        };
+            return obj;
+          };
 
-        scope.$watch("spreadsheet", function(spreadsheet) {
-          scope.defaults(spreadsheet, scope.defaultSetting);
-        });
-      }
-    };
-  }]);
+          scope.$watch("spreadsheet", function(spreadsheet) {
+            scope.defaults(spreadsheet, scope.defaultSetting);
+          });
+        }
+      };
+    }]);
+
+}());
